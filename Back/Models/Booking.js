633c45ca4e8a18eae0b1b0470d5d029ec79b9791
Andrey Miroshnichenko <andreymirosh02@gmail.com>
2024-05-16 02:51:54 +0300

    working on front

--- Back/Models/Booking.js
+++ Back/Models/Booking.js
@@ -4,7 +4,7 @@ const bookingSchema = new mongoose.Schema({
   room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
   startTime: { type: Date, required: true },
   endTime: { type: Date, required: true },
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Participant' }],
+  participant: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Participant' }],
   confirmed: { type: Boolean, default: false },
 });
 
